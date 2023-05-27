e353fcac2c2aebfdc7fa1b33c125e70377f17ea5
Kamlesh Dhankar <kamlesh.cse20@satyug.edu.in>
2023-05-27 12:41:23 +0530

    Add files via upload

---
+++ AddDataToDatabase.py
@@ -0,0 +1,57 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://face-attendance-real-tim-82c03-default-rtdb.firebaseio.com/"})
+
+
+ref = db.reference('Students')
+
+data = {
+    "322407":
+        {
+            "name": "Kamlesh ",
+            "major": "Computer Science",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 3,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "321654":
+        {
+            "name": "Murtaza Hassan",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "852741":
+        {
+            "name": "Emly Blunt",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "963852":
+        {
+            "name": "Elon Musk",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
